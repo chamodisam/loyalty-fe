7ef4e283c011036284e6e88c9db594351efea6ec
chamodisam <chamodi@codify.ai>
2025-04-17 21:42:23 +0530

    Set server port to 5175

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: 'localhost',      // Make sure it's hosted on localhost
+    port: 5175,             // Set the port to 5175
+    open: true,             // Automatically open the browser when the dev server starts
+  },
 })
